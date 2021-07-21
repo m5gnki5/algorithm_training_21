a217a3f350492be0c00217809213ccb6a516d9e5
Artyom <casually.purloined@gmail.com>
2021-07-21 14:14:38 +0300

    solutions added

--- 01i.py
+++ 01i.py
@@ -5,19 +5,19 @@ d = int(input())
 e = int(input())
 
 def sort_nums(first, second):
-	if first < second:
-		return (first, second)
-	return (second, first)
-
-a, b = sort_nums(a,b)
-b, c = sort_nums(b,c)
-a, b = sort_nums(a,b)
-d, e = sort_nums(d,e)
+    if first < second:
+        return (first, second)
+    return (second, first)
 
 if all([a, b, c, d, e]):
-	if a <= d & b <= e:
-		print("YES")
-	else:
-		print("NO")
+    a, b = sort_nums(a, b)
+    b, c = sort_nums(b, c)
+    a, b = sort_nums(a, b)
+    d, e = sort_nums(d, e)
+
+    if (a <= d) & (b <= e):
+        print("YES")
+    else:
+        print("NO")
 else:
-	print("NO")
+    print("NO")
